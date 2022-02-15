c77be741b68c9c01bff499e08ac0f7976b2a8007
Ahmedglez <ahmediglez@gmail.com>
2022-02-15 12:15:50 -0500

    change working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 const input = document.getElementById("bin-input");
 const convertButton = document.getElementById("mybutton");
 const inputShow = document.getElementById("decInput");
+const changeButton = document.getElementById("b-change");
+const label1 = document.getElementById("binNum");
+const label2 = document.getElementById("decNum");
+var myState = true;
+
+function change() {
+  if (myState === true) {
+    myState = false;
+  } else {
+    myState = true;
+  }
+  console.log(myState);
+  if (myState) {
+    label1.innerHTML = "Binary";
+    label2.innerHTML = "Decimal";
+  } else {
+    label1.innerHTML = "Decimal";
+    label2.innerHTML = "Binary";
+  }
+}
 
 convertButton.onclick = () => {
-  inputShow.value = convert_to_decimal(input.value);
+  convert(myState);
 };
 
+function convert() {
+  if (myState) {
+    inputShow.value = convert_to_decimal(input.value);
+  } else {
+    inputShow.value = convert_to_binary(input.value);
+  }
+}
+
 function convert_to_binary(integer) {
-  return integer.toString(2);
+  return parseInt(integer).toString(2);
 }
 function convert_to_decimal(binary) {
   let charArray = [];
@@ -35,3 +63,6 @@ function isValidBinary(binary) {
     return true;
   }
 }
+changeButton.onclick = () => {
+  change(myState);
+};
